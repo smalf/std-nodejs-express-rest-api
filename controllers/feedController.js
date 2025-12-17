3029c8700247814055fd611e7e2025bdbc28368c
smal_sftsrv <smal@softserveinc.com>
2025-12-17 19:29:16 +0200

    lesson(14): Added validation

--- controllers/feedController.js
+++ controllers/feedController.js
@@ -1,9 +1,20 @@
 
+const { validationResult } = require('express-validator');
+
 module.exports.getPosts = (req, res, next) => {
     res.set('Content-Type', 'application/json');
     res.status(200).json({
         posts: [
-            { title: "Post 1st", content: "This is the first post!!!" }
+            {
+                _id: 'record_1',
+                title: 'Post 1st',
+                content: 'This is the first post!!!',
+                imageUrl: 'images/fav.png',
+                creator: {
+                    name: 'Serhii M.'
+                },
+                createdAt: new Date()
+            }
         ]
     });
 }
@@ -11,6 +22,12 @@ module.exports.getPosts = (req, res, next) => {
 module.exports.createPost = (req, res, next) => {
     const title = req.body.title;
     const content = req.body.content;
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ message: 'Validation failed. ', errors: errors.array() });
+    }
 
     //Create post in DB.
     //STAUTS 201 means resource was succesfully created.
@@ -20,7 +37,11 @@ module.exports.createPost = (req, res, next) => {
         post: {
             id: new Date().toISOString(),
             title: title,
-            content: content
+            content: content,
+            creator: {
+                name: 'Serhii M.'
+            },
+            createdAt: new Date()
         }
     });
 }
--- routes/feed.js
+++ routes/feed.js
@@ -1,10 +1,25 @@
 const express = require('express');
+//check - checks value in any place request body, header, coockie etc. See documentaion.
+//body - checks value in the request body only. See documentaion. 
+const { body } = require('express-validator');
 
 const feedController = require('../controllers/feedController');
 
 const router = express.Router();
 
 router.get('/posts', feedController.getPosts);
-router.post('/post', feedController.createPost);
+router.post(
+    '/post', [
+    body('title',
+        'Title should include at list 5 characters.')
+        .trim()
+        .isLength({ min: 10 })
+    ,
+    body('content',
+        'Content should include at list 5 characters.')
+        .trim()
+        .isLength({ min: 5 })
+],
+    feedController.createPost);
 
 module.exports = router;
