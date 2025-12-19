88c91295c1ee31cb1a9ba109a22ff764b123b6bf
smal_sftsrv <smal@softserveinc.com>
2025-12-19 15:49:20 +0200

    lesson(26): Assignment refactored feedController to async/await

--- controllers/feedController.js
+++ controllers/feedController.js
@@ -36,7 +36,7 @@ module.exports.getPosts = async (req, res, next) => {
     }
 }
 
-module.exports.createPost = (req, res, next) => {
+module.exports.createPost = async (req, res, next) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -57,77 +57,72 @@ module.exports.createPost = (req, res, next) => {
     const imageUrl = req.file.path;
     const title = req.body.title;
     const content = req.body.content;
-    let creator;
 
-    const post = new Post({
-        title: title,
-        content: content,
-        imageUrl: imageUrl,
-        creator: req.userId,
-    });
-    post.save()
-        .then(result => {
-            console.log('feedController', 'createPost', 'RESULT: ' + result);
-            return User.findById(userId);
-        })
-        .then(user => {
-            creator = user;
-            user.posts.push(post);
-            return user.save();
-        })
-        .then(result => {
+    try {
+        const post = new Post({
+            title: title,
+            content: content,
+            imageUrl: imageUrl,
+            creator: req.userId,
+        });
 
-            //Create post in DB.
-            //STAUTS 201 means resource was succesfully created.
-            res.set('Content-Type', 'application/json');
-            res.status(201).json({
-                message: 'Post created successfully!',
-                post: post,
-                creator: { _id: creator._id, name: creator.name }
-            });
-        })
-        .catch(err => {
-            console.log('feedController', 'createPost', 'ERROR: ' + err);
 
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            //next will redirect an error to the top level promise. In our case it will be the root one in the app.js.
-            next(err);
-        })
+        const result = await post.save();
+        console.log('feedController', 'createPost', 'RESULT: ' + result);
 
+        const creator = await User.findById(userId);
+        creator.posts.push(post);
+
+        await creator.save();
+        //Create post in DB.
+        //STAUTS 201 means resource was succesfully created.
+        res.set('Content-Type', 'application/json');
+        res.status(201).json({
+            message: 'Post created successfully!',
+            post: post,
+            creator: { _id: creator._id, name: creator.name }
+        });
+    } catch (err) {
+        console.log('feedController', 'createPost', 'ERROR: ' + err);
+
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        //next will redirect an error to the top level promise. In our case it will be the root one in the app.js.
+        next(err);
+    }
 }
 
-module.exports.getPost = (req, res, next) => {
+module.exports.getPost = async (req, res, next) => {
     const postId = req.params.postId;
 
     console.log('feedController', 'getPost', 'postId ' + postId);
 
-    Post.findById(postId)
-        .then(post => {
-            if (!post) {
-                const error = new Error('Post with such an Id doe not esists.');
-                error.statusCode = 404;
-                throw error;
-            }
-            res.set('Content-Type', 'application/json');
-            res.status(200).json({
-                message: 'Post Fetched',
-                post: post
-            });
-        })
-        .catch(err => {
-            console.log('feedController', 'getPost', 'ERROR: ' + err);
+    try {
+        const post = await Post.findById(postId);
+        if (!post) {
+            const error = new Error('Post with such an Id doe not esists.');
+            error.statusCode = 404;
+            throw error;
+        }
 
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            //next will redirect an error to the top level promise. In our case it will be the root one in the app.js.
-            next(err);
-        })
+        res.set('Content-Type', 'application/json');
+        res.status(200).json({
+            message: 'Post Fetched',
+            post: post
+        });
+    } catch (err) {
+        console.log('feedController', 'getPost', 'ERROR: ' + err);
+
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        //next will redirect an error to the top level promise. In our case it will be the root one in the app.js.
+        next(err);
+    }
 }
 
-module.exports.updatePost = (req, res, next) => {
+module.exports.updatePost = async (req, res, next) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -137,101 +132,96 @@ module.exports.updatePost = (req, res, next) => {
         // return res.status(422).json({ message: 'Validation failed. ', errors: errors.array() });
     }
 
-    const postId = req.params.postId;
-    const title = req.body.title;
-    const content = req.body.content;
+    try {
+        const postId = req.params.postId;
+        const title = req.body.title;
+        const content = req.body.content;
 
-    let imageUrl = req.body.image;
-    if (req.file) {
-        imageUrl = req.file.path;
+        let imageUrl = req.body.image;
+        if (req.file) {
+            imageUrl = req.file.path;
+        }
+
+        if (!imageUrl) {
+            const error = new Error('No file picked.');
+            error.statusCode = 422;
+            throw error;
+        }
+
+        const post = await Post.findById(postId);
+        if (!post) {
+            const error = new Error('Post with such an Id doe not esists.');
+            error.statusCode = 404;
+            throw error;
+        }
+
+        if (post.creator.toString() !== req.userId) {
+            const error = new Error('Not authorized.');
+            error.statusCode = 403;
+            throw error;
+        }
+
+        if (imageUrl !== post.imageUrl) {
+            clearImage(post.imageUrl);
+        }
+        post.title = title;
+        post.content = content;
+        post.imageUrl = imageUrl;
+
+        const result = await post.save();
+
+        res.set('Content-Type', 'application/json');
+        res.status(200).json({
+            message: 'Post Updated',
+            post: result
+        });
+
+    } catch (err) {
+        console.log('feedController', 'putPost', 'ERROR: ' + err);
+
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        //next will redirect an error to the top level promise. In our case it will be the root one in the app.js.
+        next(err);
     }
-
-    if (!imageUrl) {
-        const error = new Error('No file picked.');
-        error.statusCode = 422;
-        throw error;
-    }
-
-    Post.findById(postId)
-        .then(post => {
-            if (!post) {
-                const error = new Error('Post with such an Id doe not esists.');
-                error.statusCode = 404;
-                throw error;
-            }
-
-            if (post.creator.toString() !== req.userId) {
-                const error = new Error('Not authorized.');
-                error.statusCode = 403;
-                throw error;
-            }
-
-            if (imageUrl !== post.imageUrl) {
-                clearImage(post.imageUrl);
-            }
-            post.title = title;
-            post.content = content;
-            post.imageUrl = imageUrl;
-            return post.save();
-        })
-        .then(result => {
-            res.set('Content-Type', 'application/json');
-            res.status(200).json({
-                message: 'Post Updated',
-                post: result
-            });
-        })
-        .catch(err => {
-            console.log('feedController', 'putPost', 'ERROR: ' + err);
-
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            //next will redirect an error to the top level promise. In our case it will be the root one in the app.js.
-            next(err);
-        })
 }
 
-module.exports.deletePost = (req, res, next) => {
-    const postId = req.params.postId;
+module.exports.deletePost = async (req, res, next) => {
+    try {
+        const postId = req.params.postId;
 
-    Post.findById(postId)
-        .then(post => {
-            if (!post) {
-                const error = new Error('Post with such an Id doe not esists.');
-                error.statusCode = 404;
-                throw error;
-            }
+        const post = await Post.findById(postId);
+        if (!post) {
+            const error = new Error('Post with such an Id doe not esists.');
+            error.statusCode = 404;
+            throw error;
+        }
 
-            if (post.creator.toString() !== req.userId) {
-                const error = new Error('Not authorized.');
-                error.statusCode = 403;
-                throw error;
-            }
+        if (post.creator.toString() !== req.userId) {
+            const error = new Error('Not authorized.');
+            error.statusCode = 403;
+            throw error;
+        }
 
-            clearImage(post.imageUrl);
-            return post.deleteOne();
-        })
-        .then(result => {
-            return User.findById(req.userId);
-        })
-        .then(user => {
-            user.posts.pull(postId);
-            return user.save();
-        })
-        .then(result => {
-            res.set('Content-Type', 'application/json');
-            res.status(200).json({
-                message: `Post ${postId} is succesfully Deleted.`
-            });
-        })
-        .catch(err => {
-            console.log('feedController', 'deletePost', 'ERROR: ' + err);
+        clearImage(post.imageUrl);
+        await post.deleteOne();
 
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            //next will redirect an error to the top level promise. In our case it will be the root one in the app.js.
-            next(err);
-        })
+        const user = await User.findById(req.userId);
+        user.posts.pull(postId);
+        await user.save();
+
+        res.set('Content-Type', 'application/json');
+        res.status(200).json({
+            message: `Post ${postId} is succesfully Deleted.`
+        });
+    } catch (err) {
+        console.log('feedController', 'deletePost', 'ERROR: ' + err);
+
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        //next will redirect an error to the top level promise. In our case it will be the root one in the app.js.
+        next(err);
+    }
 }
