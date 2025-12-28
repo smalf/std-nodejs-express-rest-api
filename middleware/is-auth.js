dedb7d831f2ec404f6e80526cbff957cc868ef15
smal_sftsrv <smal@softserveinc.com>
2025-12-28 18:27:53 +0200

    lesson(30): Added aith middleware tests

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -4,13 +4,27 @@ const { JWT_SECRET } = process.env;
 module.exports = (req, res, next) => {
     console.log("is-auth", "middleware");
     const authHeader = req.get('Authorization');
+
+    // 1. Header missing
     if (!authHeader) {
-        console.log("is-auth", "middleware", "NO authHeader");
+        console.log("is-auth", "middleware", "No Authorization Header!");
         req.isAuth = false;
-        console.log("is-auth", "middleware", "Req.isAuth = " + req.isAuth);
         return next();
     }
-    const token = authHeader.split(' ')[1];
+    // 2. Header must be a string
+    if (typeof authHeader !== 'string') {
+        req.isAuth = false;
+        return next();
+    }
+
+    // 3. Header must be "Bearer <token>"
+    const [scheme, token] = authHeader.split(' ');
+    if (scheme !== 'Bearer' || !token) {
+        req.isAuth = false;
+        return next();
+    }
+
+    // const token = authHeader.split(' ')[1];
     let decodedToken;
     try {
         decodedToken = jwt.verify(token, JWT_SECRET);
@@ -24,7 +38,7 @@ module.exports = (req, res, next) => {
         req.isAuth = false;
         return next();
     }
-    
+
     console.log("is-auth", "middleware", "All good write decoded Token. decodedToken = " + decodedToken);
     req.userId = decodedToken.userId;
     req.isAuth = true;
--- test/is-authMiddlewareTest.js
+++ test/is-authMiddlewareTest.js
@@ -2,18 +2,37 @@ const assert = require('chai').assert;
 const isAuth = require('../middleware/is-auth');
 const authMiddlleware = require('../middleware/is-auth');
 
-it('should throw an error if no authorization header is present', function() {
-    const req = {
-        get: function() {
-            return null; //Emulates the 
-        }
-    };
+describe('Auth Middleware Tests', function () {
+    it('should throw an error if no authorization header is present', function () {
+        const req = {
+            get: function () {
+                return null; //Emulates the 
+            }
+        };
 
 
-    // assert.throws(authMiddlleware.bind(this, req, {}, () => {}), 'NO authHeader')
+        // assert.throws(authMiddlleware.bind(this, req, {}, () => {}), 'NO authHeader')
 
-    authMiddlleware(req, {}, () => {});
+        authMiddlleware(req, {}, () => { });
 
-    assert.isUndefined(req.userId);
-    assert.isFalse(req.isAuth);
+        assert.isUndefined(req.userId);
+        assert.isFalse(req.isAuth);
+    });
+
+    it('should throw an error if the auth header is only 1 string (not Brearer token)', function () {
+        const req = {
+            get: function (header) {
+                return "testTokenBrearToken"; //Emulates the 
+            }
+        };
+
+        authMiddlleware(req, {}, () => { });
+
+        assert.isUndefined(req.userId);
+        assert.isFalse(req.isAuth);
+
+        // assert.throws(authMiddlleware.bind(this, req, {}, () => {}), 'Cannot read properties of testTokenBrearToken (reading \'split\')');
+        // assert.throws(authMiddlleware.bind(this, req, {}, () => { }), TypeError);
+    });
+
 });
